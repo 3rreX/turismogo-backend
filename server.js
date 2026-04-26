b1c642667846a1de9b33d38db128870cd6e5d8f0
Rodrigo Pino <rodrigopino.navarrete@gmail.com>
2026-04-26 16:58:30 -0300

    feat: configurar dominio profesional de produccion

--- server.js
+++ server.js
@@ -73,10 +73,11 @@ app.use(express.json());
 
 app.use(cors({
   origin: [
-    'http://127.0.0.1:5500',
-    'http://localhost:5500',
-    'https://turismogo-frontend.vercel.app'
-  ]
+  'http://127.0.0.1:5500',
+  'http://localhost:5500',
+  'https://turismogo-frontend.vercel.app',
+  'https://www.turismogochile.com'
+]
 }));
 // =========================
 // MODELOS
