f3d2814074e8c64a067648a3c8748f401ff7bac4
Rodrigo Pino <rodrigopino.navarrete@gmail.com>
2026-04-30 18:44:45 -0300

    Agrega dominio sin www a CORS

--- server.js
+++ server.js
@@ -195,7 +195,8 @@ const allowedOrigins = [
   'http://127.0.0.1:5500',
   'http://localhost:5500',
   'https://turismogo-frontend.vercel.app',
-  'https://www.turismogochile.com'
+  'https://www.turismogochile.com',
+  'https://turismogochile.com'
 ];
 
 app.use(cors({
